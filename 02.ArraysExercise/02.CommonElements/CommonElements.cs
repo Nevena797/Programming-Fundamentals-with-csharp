37ac5eb139e1d102a4f930bc0672913019e2cc5d
Nevena <nevena.rogova@gmx.at>
2025-03-27 08:49:28 +0200

    Add files via upload

---
+++ 02.ArraysExercise/02.CommonElements/CommonElements.cs
@@ -0,0 +1,24 @@
+﻿namespace _02.CommonElements
+{
+    internal class CommonElements
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split(" ");
+            string[] secondArray = Console.ReadLine().Split(" ");
+
+            foreach (string secondElement in secondArray)
+            {
+                for (int i = 0; i < firstArray.Length; i++)
+                {
+                    string firstElement= firstArray[i];
+                    if (firstElement == secondElement)
+                    {
+                        Console.Write(firstElement + " ");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 02.ArraysExercise/Train.cs
@@ -0,0 +1,21 @@
+﻿namespace _02.ArraysExercise
+{
+    internal class Train
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+            int[]pasengers = new int[wagons];
+            int totalPassengers = 0;
+
+            for (int i = 0; i < wagons; i++)
+            {
+                pasengers[i]= int.Parse(Console.ReadLine());
+                totalPassengers += pasengers[i];
+            }
+            Console.WriteLine(string.Join(" ", pasengers));
+            Console.WriteLine(totalPassengers);
+
+        }
+    }
+}
