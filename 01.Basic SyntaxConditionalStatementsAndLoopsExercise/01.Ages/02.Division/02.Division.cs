9a5647c648d434ce1042166d61810b0aa718c702
Nevena <nevena.rogova@gmx.at>
2025-04-04 19:53:52 +0300

    Add files via upload

---
+++ 01.Basic SyntaxConditionalStatementsAndLoopsExercise/01.Ages/02.Division/02.Division.cs
@@ -0,0 +1,40 @@
+﻿namespace _02.Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int divisor = 0;
+
+            if (number % 10 == 0)
+            {
+                divisor = 10;
+            }
+            else if (number % 7 == 0)
+            {
+                divisor = 7;
+            }
+            else if (number % 6 == 0)
+            {
+                divisor = 6;
+            }
+            else if (number % 3 == 0)
+            {
+                divisor = 3;
+            }
+            else if (number % 2 == 0)
+            {
+                divisor = 2;
+            }
+        if (divisor != 0)
+            {
+                Console.WriteLine($"The number is divisible by {divisor}");
+            }
+            else 
+            {
+                Console.WriteLine("Not divisible");
+            }
+        }
+    }
+}
