11ce31c0850b3de8c9555b0289d87d7048ba9a5c
Nevena <nevena.rogova@gmx.at>
2025-04-04 20:57:10 +0300

    Add files via upload

---
+++ 01.Basic SyntaxConditionalStatementsAndLoopsExercise/01.Ages/06.StrongNumber/06.StrongNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+        int original = num;
+        int sum = 0;
+
+        while (num > 0)
+        {
+            int digit = num % 10;
+            int fact = 1;
+
+            for (int i = 1; i <= digit; i++)
+            {
+                fact *= i;
+            }
+
+            sum += fact;
+            num /= 10;
+        }
+
+        if (sum == original)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
