feb41be0e046cc082f8d8019a0c13d84ae210b7a
Nevena <nevena.rogova@gmx.at>
2025-04-04 14:35:37 +0300

    Add files via upload

---
+++ 12.EvenNumber/12.EvenNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _12.EvenNumber;
+
+internal class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"The number is: {Math.Abs(number)}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Please write an even number.");
+            }
+        }
+    }
+}
