c304c96e3c6ee7308601963b5623474ac398fa23
Nevena <nevena.rogova@gmx.at>
2025-04-07 16:46:00 +0300

    Add files via upload

---
+++ 01.Basic SyntaxConditionalStatementsAndLoopsExercise/11.Orders/11.Orders.cs
@@ -0,0 +1,22 @@
+﻿
+    internal class Program
+    {
+        static void Main()
+        {
+            int countOfOrders = int.Parse(Console.ReadLine());
+            double total = 0;
+
+            for (int i = 0; i < countOfOrders; i++)
+            {
+                double pricePerCapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+
+                double price = pricePerCapsule * (days * capsulesCount);
+                total += price;
+                Console.WriteLine($"The price for the coffee is: ${price:F2}");
+            }
+            Console.WriteLine($"Total: ${total:F2}");
+        }
+    }
+
