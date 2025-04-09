855dca95980af261ffcb73c4909ca2e8340184c8
Nevena <nevena.rogova@gmx.at>
2025-04-09 12:21:15 +0300

    Add files via upload

---
+++ 01.Basic SyntaxConditionalStatementsAndLoopsExercise/09.SumOfOddNumbers.cs
@@ -0,0 +1,22 @@
+﻿namespace _09.SumOfOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int count = 0;
+            int oddNumber = 1;
+
+            while (count < n)
+            {
+                Console.WriteLine(oddNumber);
+                sum += oddNumber;
+                oddNumber += 2;
+                count++;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
