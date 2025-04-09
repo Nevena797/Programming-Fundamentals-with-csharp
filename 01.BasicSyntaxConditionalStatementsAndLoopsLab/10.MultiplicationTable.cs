40cebdcdc758e9b4a89af16f817029cf5d413b66
Nevena <nevena.rogova@gmx.at>
2025-04-09 12:28:57 +0300

    Add files via upload

---
+++ 01.BasicSyntaxConditionalStatementsAndLoopsLab/10.MultiplicationTable.cs
@@ -0,0 +1,17 @@
+﻿namespace _10.MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int result = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                result = i * number;
+                Console.WriteLine($"{number} X {i} = {result}");
+            }
+        }
+    }
+}
---
+++ 01.BasicSyntaxConditionalStatementsAndLoopsLab/11.MultiplicationTable2.0_1.cs
@@ -0,0 +1,18 @@
+﻿namespace _11.MultiplicationTable2._0_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());  
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine($"{numberOne} X {numberTwo} = {numberOne * numberTwo}");
+                numberTwo += 1;
+
+            } while (numberOne <= 10 && numberTwo <= 10);
+        }
+    }
+}
