d5a5ddf4d88f435955048ffafd83e3f3e6f93bef
Nevena <nevena.rogova@gmx.at>
2025-04-13 21:02:13 +0300

    Add files via upload

---
+++ 02.ExerciseDataTypesAndVariables/04.SumofChars/04.SumofChars.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _04.SumofChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberLines = int.Parse(Console.ReadLine());
+            int totalSum = 0;
+
+            for (int i = 0; i < numberLines; i++)
+            {
+                char letter = Console.ReadLine()[0];
+                int numberChar = (int)letter;
+                totalSum += numberChar;
+            }
+
+            Console.WriteLine($"The sum equals: {totalSum}");
+        }
+    }
+}
