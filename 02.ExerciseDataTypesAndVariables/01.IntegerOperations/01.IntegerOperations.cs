1821f922f45d8d6d862f9855cc543356665918ca
Nevena <nevena.rogova@gmx.at>
2025-04-13 09:54:32 +0300

    Add files via upload

---
+++ 02.ExerciseDataTypesAndVariables/01.IntegerOperations/01.IntegerOperations.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.IntegerOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            int numberTree = int.Parse(Console.ReadLine());
+            int numberFour = int.Parse(Console.ReadLine());
+
+            int result = ((numberOne + numberTwo) / numberTree) * numberFour;
+
+            Console.WriteLine(result);
+        }
+    }
+}
