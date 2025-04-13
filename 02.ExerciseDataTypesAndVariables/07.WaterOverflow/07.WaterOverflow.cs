7fe0de8fe8a36b5f14eb2b01dd1be4043ec54d00
Nevena <nevena.rogova@gmx.at>
2025-04-13 21:03:27 +0300

    Add files via upload

---
+++ 02.ExerciseDataTypesAndVariables/07.WaterOverflow/07.WaterOverflow.cs
@@ -0,0 +1,27 @@
+﻿namespace _07.WaterOverflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = 255;
+            int input = int.Parse(Console.ReadLine());
+            int currentLiters = 0;
+
+            for (int i = 0; i < input; i++)
+            {
+                int liters = int.Parse(Console.ReadLine());
+
+                if (currentLiters + liters > capacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+
+                currentLiters += liters;
+            }
+
+            Console.WriteLine(currentLiters);
+        }
+    }
+}
