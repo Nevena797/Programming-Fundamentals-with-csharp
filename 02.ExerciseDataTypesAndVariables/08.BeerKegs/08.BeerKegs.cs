e68626eb43251ea57eb3421e955fba83c5511252
Nevena <nevena.rogova@gmx.at>
2025-04-13 21:03:49 +0300

    Add files via upload

---
+++ 02.ExerciseDataTypesAndVariables/08.BeerKegs/08.BeerKegs.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace _08.BeerKegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int keg = int.Parse(Console.ReadLine());
+            double maxVolume = 0;
+            string biggestModel = "";
+
+            for (int i = 1; i <= keg; i++) 
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                double currentVolume = (Math.PI * radius * radius * height);
+
+
+                if (currentVolume > maxVolume)
+                {
+                    maxVolume = currentVolume;
+                    biggestModel = model;
+                }
+            }
+            Console.WriteLine(biggestModel);
+        }
+    }
+}
