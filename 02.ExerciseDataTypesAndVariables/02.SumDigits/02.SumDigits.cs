a2fcc6969a36d183588aaaa7e5a6b8c97dc81965
Nevena <nevena.rogova@gmx.at>
2025-04-13 21:01:12 +0300

    Add files via upload

---
+++ 02.ExerciseDataTypesAndVariables/02.SumDigits/02.SumDigits.cs
@@ -0,0 +1,19 @@
+﻿namespace _02.SumDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                
+                sum += int.Parse(number[i].ToString());
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
