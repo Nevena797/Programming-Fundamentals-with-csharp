dc2f0558324d090c80ff2b7d31c2b88ace22e3be
Nevena <nevena.rogova@gmx.at>
2025-04-13 21:02:44 +0300

    Add files via upload

---
+++ 02.ExerciseDataTypesAndVariables/05.PrintPartOfASCIITable/05.PrintPartOfASCIITable.cs
@@ -0,0 +1,23 @@
+﻿namespace _05.PrintPartOfASCIITable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= end; i++)
+            {
+                if (i != end)
+                {
+                    Console.Write((char)i + " ");
+                }
+                else 
+                {
+                    Console.Write((char)i);
+                }
+            }
+        }
+    }
+}
