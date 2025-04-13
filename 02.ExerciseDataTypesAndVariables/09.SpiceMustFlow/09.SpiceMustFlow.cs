1e8cd6937f6a8737afd9cd3bd41717b0e1b9f211
Nevena <nevena.rogova@gmx.at>
2025-04-13 21:04:08 +0300

    Add files via upload

---
+++ 02.ExerciseDataTypesAndVariables/09.SpiceMustFlow/09.SpiceMustFlow.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.Design;
+
+namespace _09.SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startYield = int.Parse(Console.ReadLine());
+            int dayMined = 0;
+            int totalMined = 0;
+
+            while (startYield >= 100)
+            {
+                totalMined += startYield - 26;
+                startYield -= 10;
+                dayMined++;
+            }
+            if (totalMined >= 26)
+            {
+                totalMined -= 26;
+            }
+            Console.WriteLine(dayMined);
+            Console.WriteLine(totalMined);
+        }
+    }
+}
