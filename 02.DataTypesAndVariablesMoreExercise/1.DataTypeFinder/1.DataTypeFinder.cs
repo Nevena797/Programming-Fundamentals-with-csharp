dd0090ae065f343f2f187ea8545ccf9f97e7e8c9
Nevena <nevena.rogova@gmx.at>
2025-04-14 08:46:37 +0300

    Add files via upload

---
+++ 02.DataTypesAndVariablesMoreExercise/1.DataTypeFinder/1.DataTypeFinder.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _1.DataTypeFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                if (int.TryParse(command, out _))
+                {
+                    Console.WriteLine($"{command} is integer type");
+                }
+                else if (float.TryParse(command, out _))
+                {
+                    Console.WriteLine($"{command} is floating point type");
+                }
+                else if (char.TryParse(command, out _))
+                {
+                    Console.WriteLine($"{command} is boolean type");
+                }
+                else
+                {
+                    Console.WriteLine($"{command} is string type");
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
