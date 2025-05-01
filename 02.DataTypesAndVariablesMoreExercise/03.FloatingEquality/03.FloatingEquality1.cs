87412d75e24b4106930a4bf70dc2316ffe9762a8
Nevena <nevena.rogova@gmx.at>
2025-05-01 12:39:00 +0300

    Add files via upload

---
+++ 02.DataTypesAndVariablesMoreExercise/03.FloatingEquality/03.FloatingEquality1.cs
@@ -0,0 +1,22 @@
+﻿namespace _03.FloatingEquality1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            double eps = 0.000001;
+
+            if (Math.Abs(firstNumber - secondNumber) >= eps )
+            {
+                Console.WriteLine("False");
+            }
+            else
+            {
+                Console.WriteLine("True");
+            }
+        }
+    }
+}
