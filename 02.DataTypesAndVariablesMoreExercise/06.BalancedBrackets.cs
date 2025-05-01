dcd26e5ba5c64a5e38664b6e6a49aac6a7e2543d
Nevena <nevena.rogova@gmx.at>
2025-05-01 15:17:35 +0300

    Add files via upload

---
+++ 02.DataTypesAndVariablesMoreExercise/06.BalancedBrackets.cs
@@ -0,0 +1,40 @@
+﻿namespace _06.BalancedBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+ 
+
+            int countOpenBrackets = 0;
+            int countCloseBrackets = 0;
+
+            for (int i = 0; i < lines; i++)
+            {
+                string input = Console.ReadLine();
+                if (input == "(")
+                {
+                    countOpenBrackets++;
+                }
+                else if (input == ")")
+                {
+                    countCloseBrackets++;
+                    if (countCloseBrackets > countOpenBrackets)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+                }
+            }
+            if (countOpenBrackets == countCloseBrackets)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
---
+++ 02.DataTypesAndVariablesMoreExercise/06.BalancedBrackets1.cs.cs
@@ -0,0 +1,46 @@
+﻿namespace _06.BalancedBrackets1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            int balance = 0;
+            bool unbalanced = false;
+
+            for (int i = 0; i < lines; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "(")
+                {
+                    balance++;
+                    if (balance > 1)
+                    {
+                        unbalanced = true;
+                        break;
+                    }
+                }
+                else if (input == ")")
+                {
+                    balance--;
+                    if (balance < 0)
+                    {
+                        unbalanced = true;
+                        break;
+                    }
+                }
+            }
+
+            if (balance == 0 && !unbalanced)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
