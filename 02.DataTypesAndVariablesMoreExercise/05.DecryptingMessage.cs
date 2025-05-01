9053408f2292dbd080f304a18f658cd25d5b4fc8
Nevena <nevena.rogova@gmx.at>
2025-05-01 14:25:03 +0300

    Add files via upload

---
+++ 02.DataTypesAndVariablesMoreExercise/05.DecryptingMessage.cs
@@ -0,0 +1,23 @@
+﻿namespace _05.DecryptingMessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int step = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            string message = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                char currentChar = Console.ReadLine()[0];
+                int asciiValue = (int)currentChar;
+                int messageValue = asciiValue + step;
+                char messageChar = (char)messageValue;
+                message += messageChar;
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
