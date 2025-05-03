bfb7b450ee5195c5ef05f06b1985735188e0a441
Nevena <nevena.rogova@gmx.at>
2025-05-03 18:36:44 +0300

    Add files via upload

---
+++ 03.ArraysExercise/06.EqualSum2.cs
@@ -0,0 +1,29 @@
+﻿namespace _06.EqualSum2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                                .Split(" ")
+                                .Select(int.Parse)
+                                 .ToArray();
+
+            int totalSum = arr.Sum();
+            int leftSum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int rightSum = totalSum - leftSum - arr[i];
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+                leftSum += arr[i];
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
