945045d8ae6da793690b32145e29be3a27335e22
Nevena <nevena.rogova@gmx.at>
2025-05-03 14:16:22 +0300

    Add files via upload

---
+++ 03.ArraysExercise/05.TopIntegers1.cs
@@ -0,0 +1,34 @@
+﻿namespace _05.TopIntegers1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = Console.ReadLine()
+                                    .Split()
+                                    .Select(int.Parse)
+                                    .ToArray();
+            
+            string topIntegers = string.Empty;
+
+            for (int i = 0; i < integers.Length; i++)
+            {
+                bool isBigger = true;
+
+                for (int k = i + 1; k < integers.Length, k++)
+                {
+                    if (integers[i] <= integers[k])
+                    {
+                        isBigger = false;
+                        break;
+                    }
+                }
+                if (isBigger)
+                {
+                    topIntegers += integers[i] + " ";
+                }
+            }
+            Console.WriteLine(topIntegers.TrimEnd());
+        }
+    }
+}
