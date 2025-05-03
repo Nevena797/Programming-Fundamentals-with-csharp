9f5ff22dc5a48dcdb480c124d44759d0cd63a8cb
Nevena <nevena.rogova@gmx.at>
2025-05-03 18:39:20 +0300

    Add files via upload

---
+++ 03.ArraysExercise/06.EqualSums.cs
@@ -0,0 +1,26 @@
+﻿namespace _06.EqualSums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                               .Split(" ")
+                               .Select(int.Parse)
+                               .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int leftSum = numbers.Take(i).Sum();
+                int rightSum = numbers.Skip(i + 1).Sum();
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
