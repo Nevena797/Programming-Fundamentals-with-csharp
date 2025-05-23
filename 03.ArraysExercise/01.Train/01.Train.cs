3d133d414a0a238a3ca632accd4666dd20babf18
Nevena <nevena.rogova@gmx.at>
2025-05-23 09:04:26 +0300

    Add files via upload

---
+++ 03.ArraysExercise/01.Train/01.Train.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+            int[] wagonsPeople = new int[wagons];
+            int sum = 0;
+
+            for (int i = 0; i < wagons; i++)
+            {
+                int people = int.Parse(Console.ReadLine());
+                wagonsPeople[i] = people;
+                sum += people;
+            }
+            Console.WriteLine(String.Join(" ", wagonsPeople));
+            Console.WriteLine(sum);
+        }
+    }
+}
