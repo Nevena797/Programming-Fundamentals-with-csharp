c5e046e324902a08c67d4bf226f0e9cecb12cef3
Nevena <nevena.rogova@gmx.at>
2025-05-23 15:56:18 +0300

    Add files via upload

---
+++ 03.ArraysMoreExercise/02.PascalTriangle1/02.PascalTriangle1.cs
@@ -0,0 +1,21 @@
+﻿namespace _02.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int n = 0; n < lines; n++)
+            {
+                int value = 1;
+                for (int k = 0; k <= n; k++)
+                {
+                    Console.Write(value + " ");
+                    value = value * (n - k) / (k + 1);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 03.ArraysMoreExercise/02.PascalTriangle2/02.PascalTriangle2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numRows = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numRows; i++)
+        {
+            long[] row = new long[i + 1];
+            row[0] = 1;
+
+            for (int j = 1; j <= i; j++)
+            {
+                row[j] = row[j - 1] * (i - j + 1) / j;
+            }
+
+            foreach (long val in row)
+            {
+                Console.Write(val + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
