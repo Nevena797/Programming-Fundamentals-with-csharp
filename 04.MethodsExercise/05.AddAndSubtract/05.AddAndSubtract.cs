112ed2a38fd401c4d1918b1cfa7e25c4d542f184
Nevena <nevena.rogova@gmx.at>
2025-05-29 10:18:20 +0300

    Add files via upload

---
+++ 04.MethodsExercise/05.AddAndSubtract/05.AddAndSubtract.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace _05.AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int thurd = int.Parse(Console.ReadLine());
+            int firstResult = Add(first, second);
+            int secondResult = Substract(firstResult,thurd);
+            Console.WriteLine(secondResult);
+
+
+        }
+        static int Add(int first, int second)
+        {
+            int firstResult = first + second;
+            return (firstResult);
+        }
+        static int Substract(int firstResult, int thurd)
+        {
+            int secondResult = firstResult - thurd;
+            return (secondResult);
+        }
+    }
+}
