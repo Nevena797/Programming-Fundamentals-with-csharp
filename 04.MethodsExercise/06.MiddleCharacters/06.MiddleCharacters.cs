d72f0a443783c60eeb08a59977d1adf874fef192
Nevena <nevena.rogova@gmx.at>
2025-05-29 17:36:17 +0300

    Add files via upload

---
+++ 04.MethodsExercise/06.MiddleCharacters/06.MiddleCharacters.cs
@@ -0,0 +1,26 @@
+﻿namespace _06.MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            PrintMiddleCharacters(input);
+        }
+
+        static void PrintMiddleCharacters(string input)
+        {
+            int lenght = input.Length;
+            int middleIndex = lenght / 2;
+
+            if (lenght % 2 == 0)
+            {
+                Console.WriteLine(input.Substring(middleIndex - 1, 2));
+            }
+            else
+            {
+                Console.WriteLine(input.Substring(middleIndex, 1));
+            }
+        }
+    }
+}
