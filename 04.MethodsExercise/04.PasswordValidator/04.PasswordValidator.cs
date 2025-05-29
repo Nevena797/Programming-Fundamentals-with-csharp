655acf0398054a62cc1c20076e897c3917227a50
Nevena <nevena.rogova@gmx.at>
2025-05-29 10:17:08 +0300

    Add files via upload

---
+++ 04.MethodsExercise/04.PasswordValidator/04.PasswordValidator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _04.PasswordValidator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            ValidatePassword(input);
+        }
+
+        public static void ValidatePassword(string input)
+        {
+            bool isLengthValid = input.Length >= 6 && input.Length <= 10;
+            bool isAlphaNumeric = true;
+            int digitCount = 0;
+
+            foreach (char c in input)
+            {
+                if (!char.IsLetterOrDigit(c))
+                {
+                    isAlphaNumeric = false;
+                }
+                if (char.IsDigit(c))
+                {
+                    digitCount++;
+                }
+            }
+
+            bool hasAtLeastTwoDigits = digitCount >= 2;
+
+            if (!isLengthValid)
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+
+            if (!isAlphaNumeric)
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+            }
+
+            if (!hasAtLeastTwoDigits)
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+            }
+
+            if (isLengthValid && isAlphaNumeric && hasAtLeastTwoDigits)
+            {
+                Console.WriteLine("Password is valid");
+            }
+        }
+    }
+}
