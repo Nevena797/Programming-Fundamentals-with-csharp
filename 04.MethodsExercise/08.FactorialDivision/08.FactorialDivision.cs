637094fd83ca45d8e3763e6cc2a37cd3d50d00c2
Nevena <nevena.rogova@gmx.at>
2025-05-29 17:37:01 +0300

    Add files via upload

---
+++ 04.MethodsExercise/08.FactorialDivision/08.FactorialDivision.cs
@@ -0,0 +1,26 @@
+﻿namespace _08.FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            long facOne = GetFactorial(numberOne);
+            long facTwo = GetFactorial(numberTwo);
+
+            double result = (double)facOne/facTwo;
+            Console.WriteLine($"{result:F2}");
+        }
+        static long GetFactorial(int number)
+        {
+            long result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
