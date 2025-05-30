4c3e63eb44ef18a6e3d0b9ba47bad05e393a0c0a
Nevena <nevena.rogova@gmx.at>
2025-05-30 13:14:58 +0300

    Add files via upload

---
+++ 04.MethodsExercise/10.TopNumber/10.TopNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TopNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (IsTopNumber(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static bool IsTopNumber(int number)
+        {
+            int sum = 0;
+            bool hasOddDigit = false;
+            int temp = number;
+
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+                sum += digit;
+
+                if (digit % 2 != 0)
+                {
+                    hasOddDigit = true;
+                }
+
+                temp /= 10;
+            }
+
+            return sum % 8 == 0 && hasOddDigit;
+        }
+    }
+}
---
+++ 04.MethodsExercise/10.TopNumber1/10.TopNumber1.cs
@@ -0,0 +1,45 @@
+﻿namespace _10.TopNumber1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] topNumbers = new int[n];
+            int count = 0;
+
+            for (int i= 0; i <= n; i++)
+            {
+                if (isTopNumber(i))
+                {
+                    topNumbers[count] = i;
+                    count++;
+                }
+            }
+            for (int i = 0; i <= count; i++)
+            {
+                Console.WriteLine(topNumbers[i]);
+            }
+        }
+        static bool isTopNumber(int number)
+        {
+            int sum = 0;
+            bool hasOddDigits = false;
+            int temp = number;
+
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+                sum += digit;
+
+                if (digit % 2 != 0)
+                {
+                    hasOddDigits = true;
+                }
+                temp /= 10;
+            }
+            return sum % 8==0 && hasOddDigits;  
+        }
+    }
+}
